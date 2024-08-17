ecda0d39246c70909aebb172c4f052700c267ed3
Lera <jgfnvb@mail.ru>
2024-08-17 14:58:01 +0700

    local work

--- src/infrastructure/mail/mail.module.ts
+++ src/infrastructure/mail/mail.module.ts
@@ -2,17 +2,14 @@ import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { mailData } from '../constants/mail.data';
 import { MailAdapter } from '../mail.adapter';
-import * as dotenv from 'dotenv';
-import * as process from 'process';
-dotenv.config();
 
 @Module({
   imports: [
     MailerModule.forRoot({
       transport: {
         host: 'smtp.gmail.com',
-        port: process.env.SMTP_PORT || 465,
-        secure: false,
+        port: 465,
+        secure: true,
         auth: {
           user: mailData.email,
           pass: mailData.pass,
